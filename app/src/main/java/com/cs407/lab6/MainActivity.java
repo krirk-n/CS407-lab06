a8bcb7dca6d23839b913c42adc1341be364e2aec
Krirk Nirunwiroj <krirk_nirunwiroj@hotmail.com>
2023-10-31 16:46:26 -0500

    milestone 1 done

--- app/src/main/java/com/cs407/lab6/MainActivity.java
+++ app/src/main/java/com/cs407/lab6/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final LatLng mDestinationLatLng = new LatLng(43.0756321,-89.4043813);
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+        });
     }
 }
